8b3640079a3ad7312ca2a3a697f861177331f333
Adebayo Wariz <adebayozoh@gmail.com>
2025-08-29 13:15:55 +0100

    Carousel Slider

--- lib/features/presentation/screens/home_screen/home_screen.dart
+++ lib/features/presentation/screens/home_screen/home_screen.dart
@@ -1,12 +1,32 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:fabrics_design/utils/constants/colors.dart';
 import 'package:fabrics_design/utils/constants/image_strings.dart';
 import 'package:fabrics_design/utils/constants/sizes.dart';
 import 'package:fabrics_design/utils/device_utils/device_utils.dart';
+import 'package:fabrics_design/utils/helper_functions/helper_functions.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class HomeScreen extends StatelessWidget {
+import '../onboarding_screen/widgets/dot_indicator.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final fabricsCarousel = [
+    CarouselImages(images: ZohImageStrings.slider1),
+    CarouselImages(images: ZohImageStrings.slider2),
+    CarouselImages(images: ZohImageStrings.slider3),
+    CarouselImages(images: ZohImageStrings.slider4),
+    CarouselImages(images: ZohImageStrings.slider5),
+    CarouselImages(images: ZohImageStrings.slider6),
+  ];
+
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -24,26 +44,24 @@ class HomeScreen extends StatelessWidget {
                       Row(
                         children: [
                           Text(
-                            'Hello',
-                            style: TextStyle(
-                              fontFamily: 'Poppins',
-                              fontSize: ZohSizes.spaceBtwZoh,
-                              fontWeight: FontWeight.bold,
-                            ),
+                              'Hello',
+                              style: GoogleFonts.sora(
+                                  fontSize: ZohSizes.defaultSpace,
+                                  fontWeight: FontWeight.bold
+                              )
                           ),
                           SizedBox(width: ZohSizes.xs),
                           Text(
-                            'Zoh',
-                            style: TextStyle(
-                              fontFamily: 'Roboto',
-                              fontWeight: FontWeight.bold,
-                              fontSize: ZohSizes.spaceBtwZoh,
-                            ),
+                              'Zoh',
+                              style: GoogleFonts.sora(
+                                  fontSize: ZohSizes.defaultSpace,
+                                  fontWeight: FontWeight.bold
+                              )
                           ),
                           SizedBox(width: ZohSizes.cardRadiusXs),
                           Image(
                             image: AssetImage(ZohImageStrings.excited),
-                            height: ZohSizes.spaceBtwSections,
+                            height: ZohSizes.spaceBtwSections * 1.2,
                           ),
                         ],
                       ),
@@ -72,6 +90,7 @@ class HomeScreen extends StatelessWidget {
                     children: [
                       Container(
                         padding: EdgeInsets.all(ZohSizes.sm),
+                        margin: EdgeInsets.only(top: ZohSizes.sm, right: ZohSizes.sm),
                         decoration: BoxDecoration(
                           border: Border.all(color: ZohColors.primaryColor),
                           borderRadius: BorderRadius.circular(ZohSizes.sm),
@@ -156,9 +175,60 @@ class HomeScreen extends StatelessWidget {
                   ),
                 ),
               ),
+              SizedBox(height: ZohSizes.spaceBtwSections),
+              Column(
+                children: [
+                  CarouselSlider(
+                    options: CarouselOptions(
+                      autoPlay: true,
+                      autoPlayCurve: Curves.fastOutSlowIn,
+                      autoPlayAnimationDuration: Duration(milliseconds: 1000),
+                      autoPlayInterval: Duration(milliseconds: 2000),
+                      enlargeCenterPage: true,
+                      onPageChanged: (index, reason) {
+                        setState(() {
+                          currentIndex = index;
+                        });
+                      },
+                    ),
+                    items: fabricsCarousel,
+                  ),
+                  SizedBox(height: ZohSizes.spaceBtwZoh,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ...List.generate(
+                        fabricsCarousel.length,
+                            (index) => Padding(
+                          padding: const EdgeInsets.only(left: ZohSizes.xs),
+                          child: DotIndicator(isActiveDot: index == currentIndex),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              )
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class CarouselImages extends StatelessWidget {
+  const CarouselImages({required this.images, super.key});
+
+  final String images;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(ZohSizes.md),
+      child: Image.asset(
+        images,
+        width: ZohHelperFunctions.screenWidth() * .9,
+        fit: BoxFit.cover,
       ),
     );
   }
--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -10,4 +10,12 @@ class ZohImageStrings {
   /// Icons Images
   static const String excited = "assets/icons/excited_icon.png";
   static const String location = "assets/icons/location_icon.png";
+
+  /// Carousel Slider
+  static const String slider1 = "assets/images/carousel_slider/image_slider1.JPG";
+  static const String slider2 = "assets/images/carousel_slider/image_slider2.JPG";
+  static const String slider3 = "assets/images/carousel_slider/image_slider3.JPG";
+  static const String slider4 = "assets/images/carousel_slider/image_slider4.JPG";
+  static const String slider5 = "assets/images/carousel_slider/image_slider5.JPG";
+  static const String slider6 = "assets/images/carousel_slider/image_slider6.JPG";
 }
