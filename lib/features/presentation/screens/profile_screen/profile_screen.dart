ca898450ce284302be79271f6bf6ddcc94953908
zoh01 <adebayozoh@gmail.com>
2025-12-11 23:05:19 +0100

    App

--- lib/features/presentation/screens/profile_screen/profile_screen.dart
+++ lib/features/presentation/screens/profile_screen/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:fabrics_design/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -7,12 +8,47 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Profile Screen"),
-      ),
-      body: Column(
-        children: [],
-      ),
+        backgroundColor: Colors.white,
+        body: Column(children: [
+          SizedBox(height: ZohSizes.md,),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: ZohSizes.md,
+              vertical: ZohSizes.sm,
+            ),
+            child: Row(
+              children: [
+                // Search Field
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(18),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 8,
+                          offset: Offset(0, 4),
+                        ),
+                      ],
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        textAlign: TextAlign.center,
+                        "Profile Screen",
+                        style: GoogleFonts.merriweather(
+                          fontSize: ZohSizes.spaceBtwZoh,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],)
     );
   }
 }
