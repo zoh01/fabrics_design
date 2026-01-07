45a87ead5880fb15067c75a1124d6f9c7c6ed403
zoh01 <adebayozoh@gmail.com>
2026-01-07 15:46:42 +0100

    Starting

--- lib/features/presentation/screens/profile_screen/profile_screen.dart
+++ lib/features/presentation/screens/profile_screen/profile_screen.dart
@@ -6,6 +6,9 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Profile Screen"),
+      ),
       body: Column(
         children: [
 
