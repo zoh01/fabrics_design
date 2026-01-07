d5e5713d907953801408b5297952e65dc9b01074
zoh01 <adebayozoh@gmail.com>
2026-01-07 15:42:31 +0100

    Circular

--- lib/features/presentation/screens/profile_screen/profile_screen.dart
+++ lib/features/presentation/screens/profile_screen/profile_screen.dart
@@ -5,6 +5,12 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
   }
 }
